df41c18e253d8e534897fb6c9e94e896f0b756c9
RlxChap2 <mmah02778@gmail.com>
2025-04-02 21:55:49 +0300

    New Changes & updates for snippets & New Discord Game (The Last Meadow)

--- snippets/TouchingGlitch.js
+++ snippets/TouchingGlitch.js
@@ -1,15 +1,38 @@
 /**
  * The Last Meadow - Touching Glitch
- * @version 1.0
- * @description: Clicks the button 20 times every 1 second
+ * @version 2.0
+ * @description: Clicks all target buttons 20 times every 1 second
  */
 
-let interval = 1;
-let threads = 20;
-let clicks = 0;
+/**
+ * @description: Auto click version 1
+ */
+async function autoClick() {
+    let interval = 1;
+    let threads = 20;
+    let clicks = 0;
 
-function autoClick() {
     let button = document.querySelector('p');
+
+    if (button) {
+        for (let i = 0; i < threads; i++) {
+            button.click();
+            clicks++;
+        }
+        console.log(`${clicks}`);
+        await sleep(interval * 1000);
+        autoClick();
+    } else {
+        console.log('❌');
+    }
+}
+
+/**
+ * @description: Auto click version 2
+ */
+function grassCollector() {
+    let button = document.querySelector('p');
+
     if (button) {
         for (let i = 0; i < threads; i++) {
             button.click();
@@ -20,6 +43,146 @@ function autoClick() {
     } else {
         console.log('❌');
     }
+}
+
+grassCollector();
+
+/**
+ * @description: Remove Mute
+ */
+function removeMute() {
+    let muteButton = document.querySelector('.image__6c989');
+
+    if (muteButton) {
+        muteButton.click();
+        console.log(`✅ Mute toggled`);
+    } else {
+        console.log('❌ Mute button not found');
+    }
+}
+
+removeMute();
+
+/**
+ * @description: Remove Flower
+ */
+function removeFlowerFunc() {
+    let muteButton = document.querySelector('.weed_fa03d7');
+
+    if (muteButton) {
+        muteButton.click();
+        console.log(`✅ Flower Removed`);
+    } else {
+        console.log('❌ Flower not found');
+    }
+}
+
+removeFlowerFunc();
+
+/**
+ * @description: Claim Leveling
+ */
+function claimLevelingButton() {
+    let muteButton = document.querySelector('.claimButton__8e695');
+
+    if (muteButton) {
+        muteButton.click();
+        console.log(`✅ Claimed Leveling Button`);
+    } else {
+        console.log('❌ Claim button not found');
+    }
+}
+
+claimLevelingButton();
+
+/**
+ * @description: Upgrade
+ */
+function upgradeButton() {
+    let muteButton = document.querySelector('.upgrade__75ed5');
+
+    if (muteButton) {
+        muteButton.click();
+        console.log(`✅ Upgraded Item`);
+    } else {
+        console.log('❌ Upgrade button not found');
+    }
+}
+
+upgradeButton();
+
+/**
+ * @description: Cart Slayer
+ */
+function cartSlayer() {
+    let muteButton = document.querySelector('.lawnmower__78658');
+
+    if (muteButton) {
+        muteButton.click();
+        console.log(`✅ Cart Slayed`);
+    } else {
+        console.log('❌ Cart not found');
+    }
+}
+
+cartSlayer();
+
+/**
+ * @description: Items
+ */
+function itemsUpgrade() {
+    let muteButton = document.querySelector('.item__4b373');
+
+    if (muteButton) {
+        muteButton.click();
+        console.log(`✅ Items Upgraded`);
+    } else {
+        console.log('❌ Items not found');
+    }
+}
+
+itemsUpgrade();
+
+/**
+ * @version 2.1
+ * @description: New System of auto click
+ */
+
+let interval = 1;
+let threads = 20;
+let clicks = 0;
+
+async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function autoClick() {
+    let selectors = [
+        'p',
+        '.image__6c989', // UnMute button
+        '.weed_fa03d7', // Flower removal
+        '.claimButton__8e695', // Claim leveling
+        '.upgrade__75ed5', // Upgrade button
+        '.lawnmower__78658', // Cart slayer
+        // '.item__4b373', // Items upgrade (it will crash your app)
+    ];
+
+    selectors.forEach((selector) => {
+        let elements = document.querySelectorAll(selector);
+        elements.forEach((element) => {
+            for (let i = 0; i < threads; i++) {
+                element.click();
+                clicks++;
+            }
+        });
+    });
+
+    setTimeout(() => {
+        console.log(`✅ Clicked ${clicks} times`);
+    }, 20 * 1000); // 20 seconds delay to check if it stils working
+
+    await sleep(interval);
+    autoClick();
 }
 
 autoClick();
--- snippets/serverAccess.js
+++ snippets/serverAccess.js
@@ -14,3 +14,47 @@ let permStore = findByProps('canBasicChannel');
     'canWithPartialContext',
     'isRoleHigher',
 ].forEach((a) => (permStore.__proto__[a] = () => true));
+
+/**
+ * @version 2
+ * @description: Server Access
+ */
+const find =
+    (typeof findByProps !== 'undefined' && findByProps) ||
+    window?.findByProps ||
+    window?.Vencord?.Webpack?.findByProps;
+if (!find)
+    throw void console.log(
+        '%cYou must run this script first: https://discord.com/channels/603970300668805120/1085682686607249478',
+        'color:red;font-size:2rem'
+    );
+
+const PermissionStore = find('canBasicChannel');
+const UserStore = find('getUserStoreVersion');
+const Guild = Object.values(find('getGuildCount').getGuilds())[0];
+
+const setProtoFields = (obj, fields, value) =>
+    fields.forEach((field) => (Object.getPrototypeOf(obj)[field] = value));
+
+setProtoFields(
+    PermissionStore,
+    [
+        'can',
+        'canAccessGuildSettings',
+        'canAccessMemberSafetyPage',
+        'canBasicChannel',
+        'canImpersonateRole',
+        'canManageUser',
+        'canWithPartialContext',
+        'isRoleHigher',
+    ],
+    () => true
+);
+
+setProtoFields(
+    Guild,
+    ['isOwner', 'isOwnerWithRequiredMfaLevel'],
+    function (id) {
+        return [UserStore.getCurrentUser()?.id, this.ownerId].includes(id);
+    }
+);
