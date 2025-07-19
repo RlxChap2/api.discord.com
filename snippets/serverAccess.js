de0339d635e13cab2d0cb26b6bf8bba75ee214fd
RlxChap2 <mmah02778@gmail.com>
2025-07-19 05:46:19 +0300

    (Fixed) ServerAccess Snippet

--- snippets/serverAccess.js
+++ snippets/serverAccess.js
@@ -1,24 +1,3 @@
-/**
- * Requiers - FindByProps
- * data/findByProps.js
- */
-
-let permStore = findByProps('canBasicChannel');
-[
-    'can',
-    'canAccessGuildSettings',
-    'canAccessMemberSafetyPage',
-    'canBasicChannel',
-    'canImpersonateRole',
-    'canManageUser',
-    'canWithPartialContext',
-    'isRoleHigher',
-].forEach((a) => (permStore.__proto__[a] = () => true));
-
-/**
- * @version 2
- * @description: Server Access
- */
 const find =
     (typeof findByProps !== 'undefined' && findByProps) || window?.findByProps || window?.Vencord?.Webpack?.findByProps;
 if (!find)
@@ -29,9 +8,15 @@ if (!find)
 
 const PermissionStore = find('canBasicChannel');
 const UserStore = find('getUserStoreVersion');
-const Guild = Object.values(find('getGuildCount').getGuilds())[0];
+const GuildStore = 'getGuildCount';
 
 const setProtoFields = (obj, fields, value) => fields.forEach((field) => (Object.getPrototypeOf(obj)[field] = value));
+const permissionProps = Object.fromEntries(
+    Object.keys(PermissionStore.getGuildPermissionProps({ id: 0 })).map((key) => [key, true])
+);
+
+setProtoFields(PermissionStore, ['getGuildPermissions'], () => ~0n);
+setProtoFields(PermissionStore, ['getGuildPermissionsProps'], (guild) => ({ ...permissionProps, guild }));
 
 setProtoFields(
     PermissionStore,
@@ -47,44 +32,7 @@ setProtoFields(
     ],
     () => true
 );
-
-setProtoFields(Guild, ['isOwner', 'isOwnerWithRequiredMfaLevel'], function (id) {
-    return [UserStore.getCurrentUser()?.id, this.ownerId].includes(id);
-});
-
-/**
- * @version 3
- * @description: Server Access
- */
-const find =
-    (typeof findByProps !== 'undefined' && findByProps) || window?.findByProps || window?.Vencord?.Webpack?.findByProps;
-if (!find)
-    throw void console.log(
-        '%cYou must run this script first: https://discord.com/channels/603970300668805120/1085682686607249478',
-        'color:red;font-size:2rem'
-    );
-
-const PermissionStore = find('canBasicChannel');
-const UserStore = find('getUserStoreVersion');
-const Guild = Object.values(find('getGuildCount').getGuilds())[0];
-
-const setProtoFields = (obj, fields, value) => fields.forEach((field) => (Object.getPrototypeOf(obj)[field] = value));
-
-setProtoFields(
-    PermissionStore,
-    [
-        'can',
-        'canAccessGuildSettings',
-        'canAccessMemberSafetyPage',
-        'canBasicChannel',
-        'canImpersonateRole',
-        'canManageUser',
-        'canWithPartialContext',
-        'isRoleHigher',
-    ],
-    () => true
+GuildStore.addChangeListener(() =>
+    GuildStore.getGuildsArray().forEach((g) => (g.ownerId = UserStore.getCurrentUser().id))
 );
-
-setProtoFields(Guild, ['isOwner', 'isOwnerWithRequiredMfaLevel'], function (id) {
-    return [UserStore.getCurrentUser()?.id, this.ownerId].includes(id);
-});
+GuildStore.emitChange();
